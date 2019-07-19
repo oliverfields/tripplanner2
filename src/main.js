6e8b06077df79de80931f92f301ed39c6022a839
Oliver Fields <oliver@phnd.net>
2019-07-19 17:14:22 +0200

    Basic authentication with Firebase working

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import firebase from 'firebase';
+import App from './App.vue'
+import router from './router'
+
+Vue.config.productionTip = false
+
+let app = '';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyB9UegF_cFN1NWlOVkUB1m7Ra70niwn_Jc",
+  authDomain: "vue-firebase-tutorial-3513b.firebaseapp.com",
+  databaseURL: "https://vue-firebase-tutorial-3513b.firebaseio.com",
+  projectId: "vue-firebase-tutorial-3513b",
+  storageBucket: "",
+  messagingSenderId: "485476877358",
+  appId: "1:485476877358:web:0c71745d612455bb"
+});
+
+firebase.auth().onAuthStateChanged(() => {
+	if(!app) {
+		app = new Vue({
+			router,
+			render: h => h(App)
+		}).$mount('#app');
+	}
+});
---
+++ src/views/Login.vue
@@ -0,0 +1,58 @@
+<template>
+	<div class="login">
+		<h3>Sign in</h3>
+		<input type="text" v-model="email" placeholder="Email"><br>
+		<input type="password" v-model="password" placeholder="Password"><br>
+		<button @click="login">Connection</button>
+		<p>I need a <router-link to="/sign-up">new account</router-link>!</p>
+	</div>
+</template>
+
+<script>
+import firebase from 'firebase';
+export default {
+	name: 'login',
+	data() {
+		return {
+			email: '',
+			password: ''
+		};
+	},
+	methods: {
+		login: function() {
+			firebase.auth().signInWithEmailAndPassword(this.email, this.password).then(
+				(user) => {
+					this.$router.replace('home')
+				},
+				(err) => {
+					alert('Nope.. ' + err.message)
+				}
+			);
+		}
+	}
+}
+</script>
+
+<style scoped>
+.login {
+margin-top: 40px;
+}
+input {
+margin: 10px 0;
+width: 20%;
+padding: 15px;
+}
+button {
+margin-top: 20px;
+widht: 10%;
+cursor: pointer;
+}
+p {
+margin-top: 40px;
+font-size: 13px;
+}
+p a {
+text-decoration: underline;
+cursor: pointer;
+}
+</style>
