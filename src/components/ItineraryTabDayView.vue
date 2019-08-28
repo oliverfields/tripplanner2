234b4bbf10dbfd47a783e5ef896246852447e108
Oliver Fields <oliver@phnd.net>
2019-08-28 21:22:13 +0200

    Saving

--- src/components/ItineraryTabDayView.vue
+++ src/components/ItineraryTabDayView.vue
@@ -3,7 +3,7 @@
 		<a href="#" @click="show({view: 'overview'})"><font-awesome-icon icon="chevron-left" /> Overview</a>
 		<h1>Day view</h1>
 		Currently editing day {{ this.$parent.day_index }}
-		<a href="#" @click="show({view: 'activity_view', day_index: '99', activity_inded: '66'})">Activity 66</a>
+		<a href="#" @click="show({view: 'activity_view', day_index: 99, activity_inded: 66})">Activity 66</a>
 	</div>
 </template>
 
--- src/components/ItineraryTabOverview.vue
+++ src/components/ItineraryTabOverview.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 		<h1>Overveiw</h1>
-		<a href="#" @click="show({view: 'day_view', day_index: '99'})">Edit day 99</a>
+		<a href="#" @click="show({view: 'day_view', day_index: 99})">Edit day 99</a>
 		<a href="#" @click="show({view: 'activity_view', day_index: 99, activity_index: 66})">Edit activity 66</a>
 	</div>
 </template>
