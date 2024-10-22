6c37a7c78e4b85d7f8a65bcf6f7888df5379f210
Oliver Fields <oliver@phnd.net>
2024-10-22 09:50:19 +0200

    tweak

--- src/router.js
+++ src/router.js
@@ -13,12 +13,10 @@ const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
-/*
     {
       path: '*',
       redirect: '/landing'
     },
-*/
     {
       path: '/',
       name: 'landing',
@@ -31,14 +29,6 @@ const router = new Router({
       meta: {
         requiresAuth: true
       }
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     },
     {
       path: '/login',
